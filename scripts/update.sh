e08d85ec3736ac93fe0ee0c31882d77264b5115e
liamtrinalizo <liamtrinalizo@live.fr>
2019-11-28 22:13:20 +0100

    updated tmux conf for 3.0

--- scripts/update.sh
+++ scripts/update.sh
@@ -16,10 +16,10 @@ eix-sync -q
 emerge -aqvuDN --keep-going=y @world
 
 echo -e "\e[32mUpdating Git repos\e[m"
-gitUpdate ~/pkg/sc-im/             
-gitUpdate ~/pkg/suckless/dmenu/    
-gitUpdate ~/pkg/suckless/dwm/      
-gitUpdate ~/pkg/suckless/slock/    
-gitUpdate ~/pkg/suckless/slstatus/ 
-gitUpdate ~/pkg/suckless/st/       
-gitUpdate ~/pkg/suckless/surf/     
+gitUpdate /home/gok/pkg/sc-im/             
+gitUpdate /home/gok/pkg/suckless/dmenu/    
+gitUpdate /home/gok/pkg/suckless/dwm/      
+gitUpdate /home/gok/pkg/suckless/slock/    
+gitUpdate /home/gok/pkg/suckless/slstatus/ 
+gitUpdate /home/gok/pkg/suckless/st/       
+gitUpdate /home/gok/pkg/suckless/surf/     
