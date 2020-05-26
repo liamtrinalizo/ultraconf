730e944b0bc3cb338f74020fcd9da4d3f2bc0795
liam <liamtrinalizo@live.fr>
2020-05-26 21:30:54 +0200

    Merge

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-rm -f \
-      ~/.bash_history \
-      ~/.gdb_history \
-      ~/.gnuplot_history \
-      ~/.lesshst \
-      ~/.octave_hist \
-      ~/.psql_history
-      ~/.python_history \
-      ~/.sqlite_history \
-      ~/.viminfo \
-      ~/.wget-hsts \
-      ~/.wpa_cli_history
+rm -fr \
+       ~/.bash_history \
+       ~/.gdb_history \
+       ~/.gnuplot_history \
+       ~/.lesshst \
+       ~/.octave_hist
+       ~/.psql_history \
+       ~/.python_history \
+       ~/.sqlite_history \
+       ~/.viminfo \
+       ~/.wget-hsts \
+       ~/.wpa_cli_history
+       ~/.wpa_cli_history \
+       ~/Document \
