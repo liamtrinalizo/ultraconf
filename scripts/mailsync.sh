5930b975f625811e62111a385e7f0198d17ae962
Liam <liamtrinalizo61@gmail.com>
2020-10-15 17:17:56 +0200

    Scripts: add unified mail sync script

---
+++ scripts/mailsync.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+gpg_status_file=/tmp/gpg_needs_key
+
+source ~/.zprofile
+
+if mbsync -c ~/.config/isync/mbsyncrc -a &>> /tmp/mbsyncrc.log; \
+	then rm -f $gpg_status_file; 
+	else touch $gpg_status_file;
+fi
