4255a81cbf38c930e861aac5cf18c2caaec16506
liam <liamtrinalizo61@gmail.com>
2020-10-22 22:57:49 +0200

    Scripts: change title color for easier reading

--- scripts/update.sh
+++ scripts/update.sh
@@ -11,10 +11,13 @@ gitUpdate()
 
 #################################
 
-echo -e "\e[32mUpdating portage\e[m"
-eix-sync -q $1
+echo -e "\e[36m--Syncing portage--\e[m"
+eix-sync -qa $1
+echo -e "\e[36m--Emerging world--\e[m"
 emerge -aqvuDN --keep-going=y --with-bdeps=y @world || exit 1
-emerge --depclean
+echo -e "\e[36m--Cleaning useless dependencies--\e[m"
+emerge --depclean --quiet
+echo -e "\e[36m--Cleaning obsolete distfiles--\e[m"
 eclean-dist
 
 #echo -e "\e[32mUpdating Git repos\e[m"
