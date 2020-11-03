675df986060e73bbd576bb178f71bcb1bcbbf1bb
liam <liamtrinalizo61@gmail.com>
2020-11-03 23:49:11 +0100

    mailsync.sh: display progress

--- scripts/mailsync.sh
+++ scripts/mailsync.sh
@@ -4,7 +4,7 @@ gpg_status_file=/tmp/gpg_needs_key
 
 source ~/.zprofile
 
-if mbsync -c ~/.config/isync/mbsyncrc -a &>> /tmp/mbsyncrc.log; \
+if mbsync -c ~/.config/isync/mbsyncrc -a 2> /tmp/mbsyncrc.log; \
 	then rm -f $gpg_status_file; 
 	else touch $gpg_status_file;
 fi
