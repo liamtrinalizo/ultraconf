6266e9f709fd1b2efd6cb5711bd6e09ec7786d3d
Liam <liamtrinalizo61@gmail.com>
2020-11-13 14:21:12 +0100

    update.sh: finished auto update of self maintained package -> brave-bin

--- scripts/update.sh
+++ scripts/update.sh
@@ -11,10 +11,15 @@ gitUpdate()
 
 #################################
 
-#TODO check for manual package update e.g. Brave, and generate ebuild
+PERSO_REPO=/var/db/repos/perso
 echo -e "\e[36m--Check for brave-broser update--\e[m"
-LATEST=$(curl https://github.com/brave/brave-browser/releases/latest | grep -Po '(?<=v)[0-9]+\.[0-9]+\.[0-9]+')
+REMOTE=$(curl -s https://github.com/brave/brave-browser/releases/latest | grep -Po '(?<=v)[0-9]+\.[0-9]+\.[0-9]+')
+LOCAL=$(INSTFORMAT='<version>' eix --pure-packages --format '<installedversions:INSTFORMAT>()\n' brave)
+[ -n $(curl -Ls https://github.com/brave/brave-browser/releases/latest | grep -Po '(?<=v)[0-9]+\.[0-9]+\.[0-9]+' | grep nightly) ] && NIGHTLY=-nightly
 
+[ "$REMOTE" = "$LOCAL" ] || cp $PERSO_REPO/www-client/brave-bin/brave-bin-{$LOCAL,$REMOTE}.ebuild && \
+                            sed -i "s@/brave-browser-@/brave-browser$NIGHTLY-@" $PERSO_REPO/www-client/brave-bin/brave-bin-$REMOTE.ebuild && \
+                            ebuild $PERSO_REPO/www-client/brave-bin/brave-bin-$REMOTE.ebuild digest
 
 echo -e "\e[36m--Syncing portage--\e[m"
 eix-sync -qa $1
@@ -25,7 +30,5 @@ emerge --depclean --quiet
 echo -e "\e[36m--Cleaning obsolete distfiles--\e[m"
 eclean-dist
 
-#echo -e "\e[32mUpdating Git repos\e[m"
-#gitUpdate /home/gok/pkg/suckless/dwm/
 #gitUpdate /home/gok/pkg/suckless/st/
 #gitUpdate /home/gok/pkg/suckless/surf/
