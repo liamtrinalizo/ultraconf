1ae2ddafd1b4baec12929ff5a037b7af3230c6eb
liam <liamtrinalizo61@gmail.com>
2020-11-22 10:51:48 +0100

    magnet.sh: rename torrent after dl

--- scripts/magnet.sh
+++ scripts/magnet.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-output=~/downloads
+outdir=~/downloads
 clipboard=$(xclip -o -selection clipboard | grep ^magnet)
 
-aria2c -d "$output" --input-file <( echo "$clipboard" ) \
+output=$(aria2c -d "$outdir" --input-file <( echo "$clipboard" ) \
                     --bt-save-metadata=true \
                     --bt-metadata-only=true \
-                    --disable-ipv6=true
+                    --disable-ipv6=true)
+
+# Rename to verbose name
+echo $(echo "$output" | grep -o '[a-z0-9/]\+.torrent') '->' $outdir/$(echo "$output" | grep -m1 -oP '(?<=METADATA]).+' | tr 'A-Z' 'a-z' | tr ' ' '_').torrent
+mv   $(echo "$output" | grep -o '[a-z0-9/]\+.torrent')      $outdir/$(echo "$output" | grep -m1 -oP '(?<=METADATA]).+' | tr 'A-Z' 'a-z' | tr ' ' '_').torrent
