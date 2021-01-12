bfe52b6a7da5b6548de7d418ead212e8ed82839c
liam <liamtrinalizo61@gmail.com>
2021-01-12 11:03:32 +0100

    jiraWorklogs.sh: remove escape chars suppression

--- scripts/jiraWorklogs.sh
+++ scripts/jiraWorklogs.sh
@@ -3,10 +3,10 @@
 DATA=$(jira list -q "worklogAuthor = $1 and worklogDate >= \"$(date -d 'last monday -7 days' +%Y-%m-%d)\"" | \
        cut -d ':' -f1 | \
        xargs -n1 jira worklog list -t debug | \
+       #sed -e's/\(\\r\?\\n\)\+/. /g' -e's/\*//g' | \
        jq -M -s "[[.[].worklogs[] | \
                select(.author.name == \"$1\")| \
-               select(.started  | strptime(\"%Y-%m-%dT%H:%M:%S.000%z\") | mktime | select(. > $(date -d 'last monday -7 days' +%s)))] | sort_by(.started) | .[] | {issueId, comment, started, timeSpent}]" | \
-               sed -e's/\(\\r\?\\n\)\+/. /g' -e's/\*//g')
+               select(.started  | strptime(\"%Y-%m-%dT%H:%M:%S.000%z\") | mktime | select(. > $(date -d 'last monday -7 days' +%s)))] | sort_by(.started) | .[] | {issueId, comment, started, timeSpent}]")
 
 function parseDuration {
     awk '{
@@ -25,14 +25,14 @@ function parseDuration {
 export -f parseDuration
 
 function parseIssueId {
-	xargs -i'%' jira list -q'issuekey = %' | cut -d':' -f1
+    xargs -i'%' jira list -q'issuekey = %' | cut -d':' -f1
 }
 export -f parseIssueId
 
 for I in $(seq 0 $(($(echo $DATA | jq 'length')-1))); do
     echo -n $(echo $DATA | jq -r ".[$I]| \"REM \(.started | strptime(\"%Y-%m-%dT%H:%M:%S.000%z\")| strftime(\"%d %b AT %H:%M\")) \""); echo -n " "
     echo -n $(echo $DATA | jq -r ".[$I]| .timeSpent" | parseDuration); echo -n " "
-	echo -n "SPECIAL COLOR 128 128 255 {"
+    echo -n "SPECIAL COLOR 128 128 255 {"
     echo -n $(echo $DATA | jq -r ".[$I]| .issueId" | parseIssueId); echo -n "} "
-	echo    $(echo $DATA | jq -r ".[$I]| .comment")
+    echo    $(echo $DATA | jq -r ".[$I]| .comment")
 done
