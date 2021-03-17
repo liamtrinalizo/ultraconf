d7a9292f25001641a93b2a2f581e64f21f86df76
liam <liamtrinalizo61@gmail.com>
2021-03-17 13:43:41 +0100

    add char to digraph list

--- scripts/digraph.sh
+++ scripts/digraph.sh
@@ -2,4 +2,4 @@
 #
 # Prompt for digraph search through dmenu
 
-dmenu -i -l 10 < "$XDG_DATA_HOME"/digraph_list | cut -d ' ' -f 1 | xclip -r
+dmenu -i -l 10 < "$XDG_DATA_HOME"/digraph_list | cut -d ' ' -f 1 | xclip -sel c -r
