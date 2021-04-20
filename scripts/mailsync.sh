d9cd9b96270edf0aa4a6a805fd1788d147730979
liam <liamtrinalizo61@gmail.com>
2021-04-20 11:12:53 +0200

    Mailcap and abook update for mutt

--- scripts/mailsync.sh
+++ scripts/mailsync.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
 gpg_status_file=/tmp/gpg_needs_key
-
 source ~/.zprofile
 
-if mbsync -c ~/.config/isync/mbsyncrc -a 2> /tmp/mbsyncrc.log; \
-	then rm -f $gpg_status_file; 
-	else touch $gpg_status_file;
-fi
+mbsync -c ~/.config/isync/mbsyncrc -a 2> /tmp/mbsyncrc.log && rm -f $gpg_status_file || touch $gpg_status_file
