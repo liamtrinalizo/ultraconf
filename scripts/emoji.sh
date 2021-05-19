3ee09be5a61a134df6db9383efccb6b85616fc71
liam <liamtrinalizo61@gmail.com>
2021-05-19 12:07:22 +0200

    emoji.sh put selected in clipboard

--- scripts/emoji.sh
+++ scripts/emoji.sh
@@ -2,4 +2,4 @@
 #
 # Prompt for emoji fuzzy search through fzy
 
-fzy < "$XDG_DATA_HOME"/emoji_list | cut -d ' ' -f 1 | xclip -r
+fzy < "$XDG_DATA_HOME"/emoji_list | cut -d ' ' -f 1 | xclip -sel c -r
