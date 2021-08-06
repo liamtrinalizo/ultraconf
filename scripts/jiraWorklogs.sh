697c8ac948712a0c8cb5c9157d4cf62ab07ee529
liam <liamtrinalizo61@gmail.com>
2021-08-06 15:17:32 +0200

    jiraWorklogs reorganize remind formating

--- scripts/jiraWorklogs.sh
+++ scripts/jiraWorklogs.sh
@@ -12,11 +12,11 @@ function parseDuration {
     awk '{
             MINUTE=0
             for (i=1; i<=NF; i++) {
-                if (substr($i, length($i), 1) == "d") 
+                if (substr($i, length($i), 1) == "d")
                     MINUTE+=substr($i, 1, length($i)-1) * 8 * 60;
-                else if (substr($i, length($i), 1) == "h") 
+                else if (substr($i, length($i), 1) == "h")
                     MINUTE+=substr($i, 1, length($i)-1) * 60;
-                else if (substr($i, length($i), 1) == "m") 
+                else if (substr($i, length($i), 1) == "m")
                     MINUTE+=substr($i, 1, length($i)-1);
             }
             print "DURATION " MINUTE
@@ -30,9 +30,9 @@ function parseIssueId {
 export -f parseIssueId
 
 for I in $(seq 0 $(($(echo $DATA | jq 'length')-1))); do
-    echo -n $(echo $DATA | jq -r ".[$I]| \"REM \(.started | strptime(\"%Y-%m-%dT%H:%M:%S.000%z\")| strftime(\"%d %b AT %H:%M\")) \""); echo -n " "
-    echo -n $(echo $DATA | jq -r ".[$I]| .timeSpent" | parseDuration); echo -n " "
-    echo -n "SPECIAL COLOR 128 128 255 {"
-    echo -n $(echo $DATA | jq -r ".[$I]| .issueId" | parseIssueId); echo -n "} "
-    echo $(echo $DATA | jq -r ".[$I]| \"\(.comment) \"" | tr [] ' ')
+    printf "REM %s %s SPECIAL COLOR 128 128 255 {%s} %s\n" \
+    "$(echo $DATA | jq -r ".[$I]| .started | strptime(\"%Y-%m-%dT%H:%M:%S.000%z\")| strftime(\"%d %b AT %H:%M\")")" \
+    "$(echo $DATA | jq -r ".[$I]| .timeSpent" | parseDuration)" \
+    "$(echo $DATA | jq -r ".[$I]| .issueId" | parseIssueId)" \
+    "$(echo $DATA | jq -r ".[$I]| .comment" | tr '[]\n' ' ')"
 done
