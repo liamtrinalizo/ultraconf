dfbf1ecec6ab66e92d4f51bad040e784340f10bf
liam <liamtrinalizo61@gmail.com>
2022-02-21 00:20:41 +0100

    screen.sh autodetect screen name

--- scripts/screen.sh
+++ scripts/screen.sh
@@ -5,17 +5,18 @@ while getopts 'l:s:' OPTION; do
         s)
             if [ $OPTARG == "1" ]; then
                 echo "Single screen mode"
-                xrandr --output LVDS1 --auto --rotate normal --pos 0x0  \
-                       --output DP1   --off
+                screen="$(xrandr | awk '/\<connected/ {print $1}')"
+                xrandr --output $screen --auto --rotate normal --pos 0x0
 
-                bspc monitor DP1   -d 1 2 3 4 5 6 7 8 9 10
+                bspc monitor $screen -d 1 2 3 4 5 6 7 8 9 10
+                bspc rule -a Brave-browser desktop='^2'
             elif [ $OPTARG == "2" ]; then
                 echo "Double screen mode"
                 xrandr --output DP1 --right-of LVDS1
 
                 bspc wm -a ALL \
-$(xrandr | awk '/\<connected/ {match($0, "[0-9]+x", arr); x+=arr[0]} END{print x}')x\
-$(xrandr | awk '/\<connected/ {match($0, "x[0-9]+", arr); num = substr(arr[0],2); if(min == 0 || min > num) min = num} END{print min}')+0+0
+                    $(xrandr | awk '/\<connected/ {match($0, "[0-9]+x", arr); x+=arr[0]} END{print x}')x \
+                    $(xrandr | awk '/\<connected/ {match($0, "x[0-9]+", arr); num = substr(arr[0],2); if(min == 0 || min > num) min = num} END{print min}')+0+0
                 bspc monitor DP1   -d 1 2 3 4 5
                 bspc monitor LVDS1 -d 6 7 8 9
                 bspc monitor ALL   -d 10
@@ -26,8 +27,8 @@ $(xrandr | awk '/\<connected/ {match($0, "x[0-9]+", arr); num = substr(arr[0],2)
                        --output DP1
 
                 bspc wm -a ALL \
-$(xrandr | awk '/\<connected/ {match($0, "[0-9]+x", arr); x+=arr[0]} END{print x}')x\
-$(xrandr | awk '/\<connected/ {match($0, "x[0-9]+", arr); num = substr(arr[0],2); if(min == 0 || min > num) min = num} END{print min}')+0+0
+                    $(xrandr | awk '/\<connected/ {match($0, "[0-9]+x", arr); x+=arr[0]} END{print x}')x \
+                    $(xrandr | awk '/\<connected/ {match($0, "x[0-9]+", arr); num = substr(arr[0],2); if(min == 0 || min > num) min = num} END{print min}')+0+0
                 bspc monitor HDMI1 -d 1 2 3
                 bspc monitor HDMI2 -d 4 5 6
                 bspc monitor DP1   -d 7 8 9
