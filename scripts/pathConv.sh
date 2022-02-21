c986dfea15ad6c92215c54ddebf7d22a0a1d5307
liam <liamtrinalizo@live.fr>
2022-02-21 17:03:43 +0100

    add cygwin path conversion

--- scripts/pathConv.sh
+++ scripts/pathConv.sh
@@ -1,13 +1,15 @@
 #!/bin/zsh
 
 printUsage() {
-    echo 'Usage pathConv.sh w|l'
+    echo 'Usage pathConv.sh w|l|c'
     echo 'w: window'
     echo 'l: linux'
+    echo 'c: cygwin'
     exit
 }
 
 [ "$#" -lt 1 ] && printUsage
 [ $1 = l ] && (d=$(< /dev/stdin tr '\\' /); echo "/mnt/${d:0:1:l}/${d:2}")
 [ $1 = w ] && (d=$(< /dev/stdin); echo "${d:5:1:u}:${${d:6}//\//\\\\}")
+[ $1 = c ] && (d=$(< /dev/stdin tr '\\' /); echo "/cygdrive/${d:0:1:l}/${d:2}")
 exit 0
