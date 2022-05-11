0e3db975baf8c10dd9d6522591ad928a759aaf4e
liam <liamtrinalizo61@gmail.com>
2022-05-11 13:36:19 +0200

    fix mailsync error redirection

--- scripts/mailsync.sh
+++ scripts/mailsync.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 gpg_status_file=/tmp/gpg_needs_key
-imaptool=$(which mbsync 2>/dev/null && echo mbsync || echo offlineimap)
+imaptool=$(which mbsync >/dev/null && echo mbsync || echo offlineimap)
 
 # mbsync needs the -a parameter
-toolparam=$(which mbsync 2>/dev/null && echo -a)
+toolparam=$(which mbsync >/dev/null && echo -a)
 source ~/.zprofile
 
 $imaptool -c $XDG_CONFIG_HOME/$imaptool/config $toolparam 2> /tmp/mailsync.log && rm -f $gpg_status_file || touch $gpg_status_file
